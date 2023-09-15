3762af00a7df08b26e6a60dcdfdfa970725cdfd3
[Sbarito] <[2b1000okn35@gmail.com]>
2023-09-16 00:40:13 +0300

    delete build

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/OOP_1_lab/googletest
-# Build directory: C:/OOP_1_lab/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
